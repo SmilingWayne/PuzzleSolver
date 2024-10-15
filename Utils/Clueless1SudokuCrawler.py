cf09d4001c5b0aba0e05d34bb620accbc65c66cf
xiaoxiao <593302270@qq.com>
2024-10-15 23:53:28 +0800

    Add dataset and Update CN/EN README

--- Utils/Clueless1SudokuCrawler.py
+++ Utils/Clueless1SudokuCrawler.py
@@ -2,12 +2,11 @@ import requests
 import re
 import time
 
-# NEED TO ADD DATA
-
-def get_clueless2_sudoku(problems):
+def get_clueless1_sudoku(problems):
     for p in problems:
         new_p = str(p).zfill(3)
-        target_url = f"https://www.janko.at/Raetsel/Sudoku/Clueless-2/{new_p}.a.htm"
+        
+        target_url = f"https://www.janko.at/Raetsel/Sudoku/Clueless-1/{new_p}.a.htm"
         headers = {
             'User-Agent': "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
             'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -29,10 +28,7 @@ def get_clueless2_sudoku(problems):
 
         problem_pattern = r"(?<=\[problem\]\n)(.*?)(?=\[solution\])"
         # 正则表达式提取 [solution] 和 [moves] 之间的内容
-        solution_pattern = r"(?<=\[solution\]\n)(.*?)(?=\[end\])"
-        
-        # NOTE：Some puzzles the solution pattern ends with "end", some end with "moves". So better check the output, especially:
-        # Solution!
+        solution_pattern = r"(?<=\[solution\]\n)(.*?)(?=\[moves\])"
 
         # 使用 re.DOTALL 使 '.' 匹配换行符
         problem_text = re.search(problem_pattern, page_source, re.DOTALL).group().strip()
@@ -57,14 +53,14 @@ def get_clueless2_sudoku(problems):
         print(f"SIZE: r = {num_rows}, c = {num_cols}")
         # print(problem_text)
 
-        with open(f"../assets/Sudoku/Clueless2Sudoku/problems/{p}_{num_rows}x{num_cols}.txt", "w") as file:
+        with open(f"../assets/Sudoku/Clueless1Sudoku/problems/{p}_{num_rows}x{num_cols}.txt", "w") as file:
             # 写入行数和列数到第一行
             file.write(f"{num_rows} {num_cols}\n")
             
             # 写入 problem_text 的每一行
             file.write(problem_text + '\n')
         
-        with open(f"../assets/Sudoku/Clueless2Sudoku/solutions/{p}_{num_rows}x{num_cols}.txt", "w") as file:
+        with open(f"../assets/Sudoku/Clueless1Sudoku/solutions/{p}_{num_rows}x{num_cols}.txt", "w") as file:
             # 写入行数和列数到第一行
             file.write(f"{num_rows} {num_cols}\n")
             
@@ -75,5 +71,5 @@ def get_clueless2_sudoku(problems):
         time.sleep(2)
     
 if __name__ == "__main__":
-    problems = [19, 11, 24, 29, 26, 5, 28, 8, 10, 17, 13, 9, 7, 6, 27, 2, 4, 15, 20, 30, 12]
-    get_clueless2_sudoku(problems)
+    problems = [29, 21, 19, 27, 23, 26, 25, 15, 30, 14, 28, 18, 24, 10, 2, 9, 11, 1, 16, 13, 6, 7, 5, 17, 8, 20, 3, 12, 4]
+    get_clueless1_sudoku(problems)
--- Utils/EvenOddSudokuCrawler.py
+++ Utils/EvenOddSudokuCrawler.py
@@ -4,10 +4,10 @@ import time
 
 # NEED TO ADD DATA
 
-def get_clueless2_sudoku(problems):
+def get_evenodd_sudoku(problems):
     for p in problems:
         new_p = str(p).zfill(3)
-        target_url = f"https://www.janko.at/Raetsel/Sudoku/Clueless-2/{new_p}.a.htm"
+        target_url = f"https://www.janko.at/Raetsel/Sudoku/Odd-Even/{new_p}.a.htm"
         headers = {
             'User-Agent': "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
             'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -45,26 +45,38 @@ def get_clueless2_sudoku(problems):
             except Exception:
                 solution_text = ""
         rows = problem_text.split("\n")
+        rows_ans = solution_text.split("\n")
 
         # 解析每行的列（通过空格分割每行）
         matrix = [row.split() for row in rows]
-
+        matrix_ans = [row.split() for row in rows_ans]
         # 行数
         num_rows = len(matrix)
 
         # 列数 (假设每行列数一致)
         num_cols = len(matrix[0]) if num_rows > 0 else 0
         print(f"SIZE: r = {num_rows}, c = {num_cols}")
-        # print(problem_text)
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if matrix[i][j] in "123456789" or matrix_ans[i][j] == "-":
+                    continue
+                if int(matrix_ans[i][j]) % 2 == 0:
+                    matrix[i][j] = 'E'
+                else:
+                    matrix[i][j] = 'O'
 
-        with open(f"../assets/Sudoku/Clueless2Sudoku/problems/{p}_{num_rows}x{num_cols}.txt", "w") as file:
+        with open(f"../assets/Sudoku/EvenOddSudoku/problems/{p}_{num_rows}x{num_cols}.txt", "w") as file:
             # 写入行数和列数到第一行
             file.write(f"{num_rows} {num_cols}\n")
             
             # 写入 problem_text 的每一行
-            file.write(problem_text + '\n')
+            for idx, each_row in enumerate(matrix):
+                if idx < num_rows - 1:
+                    file.write(" ".join(each_row) + '\n')
+                else:
+                    file.write(" ".join(each_row))
         
-        with open(f"../assets/Sudoku/Clueless2Sudoku/solutions/{p}_{num_rows}x{num_cols}.txt", "w") as file:
+        with open(f"../assets/Sudoku/EvenOddSudoku/solutions/{p}_{num_rows}x{num_cols}.txt", "w") as file:
             # 写入行数和列数到第一行
             file.write(f"{num_rows} {num_cols}\n")
             
@@ -75,5 +87,5 @@ def get_clueless2_sudoku(problems):
         time.sleep(2)
     
 if __name__ == "__main__":
-    problems = [19, 11, 24, 29, 26, 5, 28, 8, 10, 17, 13, 9, 7, 6, 27, 2, 4, 15, 20, 30, 12]
-    get_clueless2_sudoku(problems)
+    problems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,  113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 134, 135, 136, 137, 138, 139, 140]
+    get_evenodd_sudoku(problems)

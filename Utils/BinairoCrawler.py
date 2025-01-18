3f1f50786a3970173d9bfe057f28080382190397
xiaoxiao <593302270@qq.com>
2025-01-18 16:33:53 +0800

    Update Tent code, Binairo & Gokigen Naname dataset

--- Utils/BinairoCrawler.py
+++ Utils/BinairoCrawler.py
@@ -3,20 +3,22 @@ import re
 import time
 from common import get_exist
 
-def get_fobidoshi(problems):
-    exist_files_list = get_exist("../assets/data/Fobidoshi/problems/")
+
+def get_binairo(problems):
+
+    exist_files_list = get_exist("../assets/data/Binairo/problems/")
     exist_files = set()
     for fileName in exist_files_list:
         tmp = fileName.split("_")[0]
         exist_files.add(tmp)
 
     for p in problems:
+        input_p = str(p).zfill(3)
         if str(p) in exist_files:
             print("JUMP!")
             continue
-        exist_files.add(str(p))    
-        temp_p = str(p).zfill(3)
-        target_url = f"https://www.janko.at/Raetsel/Fobidoshi/{temp_p}.a.htm"
+        exist_files.add(str(p))
+        target_url = f"https://www.janko.at/Raetsel/Tohu-Wa-Vohu/{input_p}.a.htm"
 
         headers = {
             'User-Agent': "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
@@ -26,7 +28,7 @@ def get_fobidoshi(problems):
             'Connection': "keep-alive",
             'Cookie': "index=2; rules=2; genre=2; https__www_janko_at_Raetsel_Slitherlink_0312_a_htm_0=aFxkQXTcYdfwiUew",
             'Host': "www.janko.at",
-            'Referer': "https://www.janko.at/Raetsel/index.htm",
+            'Referer': "https://www.janko.at/Raetsel/Slitherlink/index.htm",
             'Sec-Fetch-Dest': "document",
             'Sec-Fetch-Mode': "navigate",
             'Sec-Fetch-Site': "same-origin"
@@ -35,7 +37,8 @@ def get_fobidoshi(problems):
         response = requests.get(target_url, headers=headers)     
         response.encoding = 'utf-8'
         page_source = response.text
-        
+        # print(page_source)
+
         problem_pattern = r"(?<=\[problem\]\n)(.*?)(?=\[solution\])"
         # 正则表达式提取 [solution] 和 [moves] 之间的内容
         solution_pattern = r"(?<=\[solution\]\n)(.*?)(?=\[moves\])"
@@ -51,37 +54,37 @@ def get_fobidoshi(problems):
             except Exception:
                 solution_text = ""
 
-
         rows = problem_text.split("\n")
 
         # 解析每行的列（通过空格分割每行）
         matrix = [row.split() for row in rows]
 
         # 行数
-        num_rows = len(matrix) - 1
+        num_rows = len(matrix)
 
         # 列数 (假设每行列数一致)
-        num_cols = len(matrix[0]) - 1 if num_rows > 0 else 0
+        num_cols = len(matrix[0]) if num_rows > 0 else 0
         print(f"SIZE: r = {num_rows}, c = {num_cols}")
 
-        with open(f"../assets/data/Fobidoshi/problems/{p}_{num_rows}x{num_cols}.txt", "w") as file:
+        with open(f"../assets/data/Binairo/problems/{p}_{num_rows}x{num_cols}.txt", "w") as file:
             # 写入行数和列数到第一行
             file.write(f"{num_rows} {num_cols}\n")
-
+            
+            # 写入 problem_text 的每一行
             file.write(problem_text)
         
-        with open(f"../assets/data/Fobidoshi/solutions/{p}_{num_rows}x{num_cols}.txt", "w") as file:
+        with open(f"../assets/data/Binairo/solutions/{p}_{num_rows}x{num_cols}.txt", "w") as file:
             # 写入行数和列数到第一行
             file.write(f"{num_rows} {num_cols}\n")
+            
             # 写入 problem_text 的每一行
             file.write(solution_text)
-
+        
         print(f"FILE: problems/{p}_{num_rows}x{num_cols}.txt and FILE solutions/{p}_{num_rows}x{num_cols}.txt, done!")
         time.sleep(2)
-    
+
 if __name__ == "__main__":
-    problems = [31, 32, 41, 42, 71, 72,1, 2, 3, 4, 33, 34, 51, 52, 61, 62, 73, 74, 81, 91, 111, 112, 121, 122, 131, 132, 141, 142, 151, 152, 191, 192, 201, 202, 211, 212,21, 22, 43, 44, 75, 76, 82, 83, 92, 93, 113, 114, 123, 124,5, 6, 7, 8, 23, 24, 35, 36, 45, 46, 53, 54, 63, 64, 77, 78, 84, 85, 94, 95, 101, 102, 115, 116, 125, 126, 133, 134, 135, 143, 144, 145, 153, 154, 155, 161, 162, 163, 171, 172, 173, 181, 182, 183, 193, 194, 203, 204, 213, 214, 221, 222, 223, 231, 232, 233,9, 10, 11, 12, 25, 26, 55, 56, 65, 66, 86, 87, 96, 97, 103, 104, 117, 118, 127, 128,13, 14, 15, 16, 27, 28, 37, 38, 47, 48, 57, 58, 67, 68, 88, 89, 90, 98, 99, 100, 105, 106, 107, 109, 110, 119, 120, 129, 130, 136, 137, 138, 146, 147, 148, 156, 157, 158, 164, 165, 166, 167, 168, 174, 175, 176, 177, 178, 184, 185, 186, 187, 188, 195, 196, 197, 205, 206, 207, 215, 216, 217, 224, 225, 226, 227, 229, 230, 234, 235, 236, 237,17, 18, 19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 70, 79, 80, 108, 139, 140, 149, 150, 159, 160, 169, 170, 179, 180, 189, 190, 198, 199, 200, 208, 209, 210, 218, 219, 220, 228, 238, 239, 240]
-    get_fobidoshi(problems)
-    
-    
-    
+    # problems = [131, 132, 141, 142, 261, 262, 271, 272, 281, 282, 301, 302, 311, 312, 321, 322, 331, 332, 341, 342, 351, 352, 361, 362, 371, 372, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 133, 134, 135, 136, 143, 144, 145, 146, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 263, 264, 265, 273, 274, 275, 283, 284, 285, 291, 292, 293, 303, 304, 305, 313, 314, 315, 323, 324, 328, 333, 334, 335, 343, 344, 345, 353, 354, 355, 363, 364, 365, 373, 374, 375]
+    problems = [137, 138, 147, 148, 266, 267, 268, 276, 277, 278, 286, 287, 288, 294, 295, 296, 297, 298, 306, 307, 308, 316, 317, 318, 325, 326, 327, 336, 337, 338, 346, 347, 348, 356, 357, 358, 366, 367, 368, 376, 377, 378,139, 140, 149, 150, 269, 270, 279, 280, 289, 290, 299, 300, 309, 310, 319, 320, 329, 330, 339, 340, 349, 350, 359, 360, 369, 370, 379, 380]
+    # TODO not done!!
+    get_binairo(problems)

4033e94d2a1fe06fe606ba54028b7cd9c39f23b9
xiaoxiao <xiaoxiaowayne@163.com>
2025-11-22 18:42:24 +0800

    Update factory functions and unzip Tent files

---
+++ Puzzles/Common/Parser/ParserFactory.py
@@ -0,0 +1,29 @@
+from Common.Parser.PuzzleParsers.AkariParser import AkariParser
+from Common.Parser.PuzzleParsers.ShikakuParser import ShikakuParser
+from Common.Parser.PuzzleParsers.TentParser import TentParser
+
+class ParserFactory:
+    """Puzzle parser factory """
+    
+    _parsers = {
+        'Akari': AkariParser,
+        'Shikaku': ShikakuParser,
+        'Tent': TentParser,
+    }
+    
+    @classmethod
+    def get_parser(cls, puzzle_name: str):
+        """Get parser via name"""
+        parser_class = cls._parsers.get(puzzle_name)
+        if parser_class:
+            return parser_class()
+        raise ValueError(f"Unable to find parse of Puzzle '{puzzle_name}'.")
+    
+    @classmethod
+    def register_parser(cls, puzzle_name: str, parser_class):
+        """Register new parser"""
+        cls._parsers[puzzle_name] = parser_class
+    
+    @classmethod
+    def get_available_parsers(cls):
+        return list(cls._parsers.keys())

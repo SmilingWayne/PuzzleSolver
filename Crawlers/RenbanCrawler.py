7b633a4e6937e9727a6e5735d4fbf89db7f86ea9
xiaoxiao <xiaoxiaowayne@163.com>
2025-12-05 17:51:23 +0800

    Update dataset: EntryExit, Nondango, Simpleloop, patchwork etc.

--- Crawlers/RenbanCrawler.py
+++ Crawlers/RenbanCrawler.py
@@ -9,7 +9,7 @@ from typing import Any
 import random
 import json
 
-class StarbattleCrawler(GridCrawler):
+class RenbanCrawler(GridCrawler):
     def __init__(self, data : dict[str, Any]):
         self._data = data 
         self.puzzle_name = self._data['puzzle_name'] 
@@ -98,14 +98,15 @@ class StarbattleCrawler(GridCrawler):
                     text_ = dic['text']
                     
                     if type_ == "class_sv":
+                        area_pattern = r"(?<=\[problem\]\n)(.*?)(?=\[areas\])"
                         problem_pattern = r"(?<=\[areas\]\n)(.*?)(?=\[solution\])"
                         solution_pattern = r"(?<=\[solution\]\n)(.*?)(?=\[moves\])"
                     elif type_ == "no_class_sv":
-                        problem_pattern = r"(?<=\[labels\]\n)(.*?)(?=\[solution\])"
+                        area_pattern = r"(?<=\[problem\]\n)(.*?)(?=\[areas\])"
+                        problem_pattern = r"(?<=\[problem\]\n)(.*?)(?=\[solution\])"
                         solution_pattern = r"(?<=\[solution\]\n)(.*?)(?=\[end\])"
                     else:
                         continue
-                    
                     
                     target_url = f"{self.root_url}{href_}"
 
@@ -114,23 +115,19 @@ class StarbattleCrawler(GridCrawler):
                     
                     page_source = response.text
 
+                    area_text = re.search(area_pattern, page_source, re.DOTALL).group().strip()
                     problem_text = re.search(problem_pattern, page_source, re.DOTALL).group().strip()
                     solution_text = re.search(solution_pattern, page_source, re.DOTALL).group().strip()
 
-                    
-                    rows = solution_text.split("\n")
+                    rows = problem_text.split("\n")
                     matrix = [row.split() for row in rows]
-                    k = 0
-                    for r_ in matrix[0]:
-                        if r_ == "x":
-                            k += 1
-                            
+
                     num_rows = len(matrix)
                     num_cols = len(matrix[0]) if num_rows > 0 else 0
                     
                     pzl_name = f"{text_}_{num_rows}x{num_cols}"
-                    problem_str = f"{num_rows} {num_cols} {k}\n{problem_text}"
-                    solution_str = f"{num_rows} {num_cols} {k}\n{solution_text}"
+                    problem_str = f"{num_rows} {num_cols}\n{area_text}\n{problem_text}"
+                    solution_str = f"{num_rows} {num_cols}\n{solution_text}"
                     
                     puzzles_ret['puzzles'][pzl_name] = {
                         "id": pzl_name, 
